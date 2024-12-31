f8d15b86b80b71489199510e863e9c8a804546bb
diamond_dai <diamond.dai@gmail.com>
2024-12-30 23:28:28 -0700

    test on mongodb connection

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	//"errors"
@@ -116,14 +117,33 @@ func Connect() *mongo.Collection {
 	return collection
 }
 
-func dbconnect_test() {
-
-}
-
 const uri = "mongodb://localhost:27017/test"
 
+func dbconnect_test() {
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Send a ping to confirm a successful connection
+	var result bson.M
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+}
+
 func main() {
-	Connect()
+	dbconnect_test()
 	router := gin.Default()
 	router.GET("/questions", getQuestions)
 	router.GET("/comments", getComments)
