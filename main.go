3b6f03f35631d901c56a99c00a2485b67ebd62ba
diamond_dai <diamond.dai@gmail.com>
2025-01-05 00:23:32 -0700

    Completed the basic query endpoints sample

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	//"errors"
 )
 
-type question struct {
+type Question struct {
 	QuestionID   string `json:"questionId"`
 	QuestionType string `json:"questionType"`
 	QuestionText string `json:"questionText"`
@@ -27,7 +27,8 @@ type Comment struct {
 	CommentText string `json:"commentText"`
 }
 
-var questions = []question{
+// data stored in memory and endpoint testing
+var questions = []Question{
 	{QuestionID: "1",
 		QuestionText: "The genealogy of Jesus Christ in the Gospel of Matthew begins with Adam.",
 		Answer:       false,
@@ -79,12 +80,7 @@ func getComments(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, comments)
 }
 
-// func getQueryComment(c *gin.Context) {
-// 	// convert bson.M to Struct
-// 	var queryComment comment
-// 	var m = c.IndentedJSON(http.StatusOK, comments)
-// }
-
+// data from MongoDB
 // This function closes mongoDB connection and cancel context
 func close(client *mongo.Client, ctx context.Context, cancel context.CancelFunc) {
 	defer cancel()
@@ -95,12 +91,95 @@ func close(client *mongo.Client, ctx context.Context, cancel context.CancelFunc)
 	}()
 }
 
-func connect(uri string) (*mongo.Client, context.Context,
-	context.CancelFunc, error) {
-	ctx, cancel := context.WithTimeout(context.Background(),
-		30*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
-	return client, ctx, cancel, err
+// This is basic function connect mongoDB
+// func connectDB(url string) *mongo.Client {
+// 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+// 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(url))
+// 	if err != nil {
+// 		panic(fmt.Errorf("Failed to connect to MongoDB: %w", err))
+// 	}
+// 	return client
+// }
+
+// Function of getting client and collection according to url, dbname and collection name
+func getMongoDBConnection(dbname string, collectname string) (*mongo.Client, *mongo.Collection, error) {
+	// connect mongoDB
+	var url = "mongodb://localhost:27017"
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(url))
+	if err != nil {
+		return nil, nil, fmt.Errorf("Failed to connect to MongoDB: %w", err)
+	}
+
+	// Get the database and collection
+	db := client.Database(dbname)
+	collection := db.Collection(collectname)
+	return client, collection, nil
+}
+
+// Function of getting all comments
+func getAllComments(c *gin.Context) {
+	// Get the MongoDB client and collection
+	client, collection, err := getMongoDBConnection("local", "comments")
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	defer client.Disconnect(context.Background())
+
+	// Retrieve all records from MongoDB
+	cursor, err := collection.Find(context.Background(), bson.M{})
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	defer cursor.Close(context.Background())
+
+	// Convert the cursor to a slice of records
+	var allComments []Comment
+	for cursor.Next(context.Background()) {
+		var comment Comment
+		if err := cursor.Decode(&comment); err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		allComments = append(allComments, comment)
+	}
+
+	c.IndentedJSON(http.StatusOK, allComments)
+}
+
+// Function of getting a specific comment according to comment id
+func getCommentByCommentID(c *gin.Context) {
+	// Get the comment ID from the request URL
+	commentid := c.Query("commentid")
+
+	// Get the MongoDB client and collection
+	client, collection, err := getMongoDBConnection("local", "comments")
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	defer client.Disconnect(context.Background())
+
+	// Retrieve the comment from MongoDB
+	cursor, err := collection.Find(context.Background(), bson.M{"commentid": commentid})
+	if err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	defer cursor.Close(context.Background())
+
+	var comments []Comment
+	for cursor.Next(context.Background()) {
+		var comment Comment
+		if err := cursor.Decode(&comment); err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		comments = append(comments, comment)
+	}
+	c.IndentedJSON(http.StatusOK, comments)
 }
 
 // query method returns a cursor and error.
@@ -117,57 +196,14 @@ func query(client *mongo.Client, ctx context.Context,
 	return
 }
 
-// get method for endpoint
-func getData() (comments []bson.D) {
-	// Get Client, Context, CancelFunc and err from connect method.
-	client, ctx, cancel, err := connect("mongodb://localhost:27017")
-	if err != nil {
-		panic(err)
-	}
-
-	// Free the resource when main function is returned
-	defer close(client, ctx, cancel)
-
-	// create a filter an option of type interface,
-	// that stores bjson objects.
-	var filter, option interface{}
-
-	// filter  gets all document,
-	// with maths field greater that 70
-	filter = bson.D{{"commentid", "22749003"}}
-
-	//  option remove id field from all documents
-	option = bson.D{{"_id", 0}}
-
-	// call the query method with client, context, database name, collection  name, filter and option
-	cursor, err := query(client, ctx, "local",
-		"comments", filter, option)
-	// handle the errors.
-	if err != nil {
-		panic(err)
-	}
-
-	var results []bson.D
-
-	if err := cursor.All(ctx, &results); err != nil {
-		// handle the error
-		panic(err)
-	}
-
-	// printing the result of query.
-	fmt.Println("Query Result")
-	for _, doc := range results {
-		fmt.Println(doc)
-	}
-	return results
-}
-
 // main function
 func main() {
-	getData()
-	// router := gin.Default()
-	// router.GET("/questions", getQuestions)
-	// router.GET("/comments", getComments)
-	// router.POST("/comment", createComment)
-	// router.Run("localhost:8080")
+	// Create a new Gin router
+	router := gin.Default()
+
+	// Define the route to retrieve all records
+	router.GET("/comments", getAllComments)
+	router.GET("/comments/search", getCommentByCommentID)
+	// Run the router
+	router.Run("localhost:8080")
 }
--- test.go
+++ test.go
@@ -1,63 +1,63 @@
-package main
+// package main
 
-import (
-	"context"
-	"fmt"
-	"net/http"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"net/http"
 
-	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-)
+// 	"github.com/gin-gonic/gin"
+// 	"go.mongodb.org/mongo-driver/bson"
+// 	"go.mongodb.org/mongo-driver/mongo"
+// 	"go.mongodb.org/mongo-driver/mongo/options"
+// )
 
-type Comment struct {
-	CommentID   string `json:"commentid"`
-	CommentText string `json:"commentText"`
-}
+// type Comment struct {
+// 	CommentID   string `json:"commentid"`
+// 	CommentText string `json:"commentText"`
+// }
 
-func main() {
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		panic(fmt.Errorf("Failed to connect to MongoDB: %w", err))
-	}
-	defer client.Disconnect(context.Background())
+// func main() {
+// 	// Connect to MongoDB
+// 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+// 	if err != nil {
+// 		panic(fmt.Errorf("Failed to connect to MongoDB: %w", err))
+// 	}
+// 	defer client.Disconnect(context.Background())
 
-	// Get the database and collection
-	db := client.Database("local")
-	collection := db.Collection("comments")
+// 	// Get the database and collection
+// 	db := client.Database("local")
+// 	collection := db.Collection("comments")
 
-	// Create a new Gin router
-	r := gin.Default()
+// 	// Create a new Gin router
+// 	r := gin.Default()
 
-	// Define the route to retrieve a comment
-	r.GET("/comments", func(c *gin.Context) {
-		// Retrive all the comments from the request URL
+// 	// Define the route to retrieve a comment
+// 	r.GET("/comments", func(c *gin.Context) {
+// 		// Retrive all the comments from the request URL
 
-		cursor, err := collection.Find(context.Background(), bson.M{})
-		if err != nil {
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
+// 		cursor, err := collection.Find(context.Background(), bson.M{})
+// 		if err != nil {
+// 			c.AbortWithStatus(http.StatusInternalServerError)
+// 			return
+// 		}
 
-		defer cursor.Close(context.Background())
+// 		defer cursor.Close(context.Background())
 
-		// Convert the cursor to a slice of records
-		var comments []Comment
-		for cursor.Next(context.Background()) {
-			var comment Comment
-			if err := cursor.Decode(&comment); err != nil {
-				c.AbortWithStatus(http.StatusInternalServerError)
-				return
-			}
-			comments = append(comments, comment)
-		}
+// 		// Convert the cursor to a slice of records
+// 		var comments []Comment
+// 		for cursor.Next(context.Background()) {
+// 			var comment Comment
+// 			if err := cursor.Decode(&comment); err != nil {
+// 				c.AbortWithStatus(http.StatusInternalServerError)
+// 				return
+// 			}
+// 			comments = append(comments, comment)
+// 		}
 
-		// Return the records as JSON
-		c.JSON(http.StatusOK, comments)
-	})
+// 		// Return the records as JSON
+// 		c.JSON(http.StatusOK, comments)
+// 	})
 
-	// Start the server
-	r.Run(":8080")
-}
+// 	// Start the server
+// 	r.Run(":8080")
+// }
